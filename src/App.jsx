4e23d5693b4c42bb4c71089bda9be2b46c5a6589
YanivLegin <leginyaniv@post.iac.ac.il>
2026-07-10 11:22:04 +0300

    Fix white screen on deploy: Switch to HashRouter and add FontAwesome CDN

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
 import Dashboard from './pages/Dashboard';
 import SubjectView from './pages/SubjectView';
